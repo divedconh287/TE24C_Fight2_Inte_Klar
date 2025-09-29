d124194b5748060dad5ca1a697c20edd670c2abb
Aaron Granath TE24C <divedconh287@gmail.com>
2025-09-29 07:54:08 +0200

    kopierade in TE24C Fight 1 koden, nu ska man ba' förbättra den

--- TE24C_Fight2/Program.cs
+++ TE24C_Fight2/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int prohp = 100;
+int anthp = 100;
+int NO = 1;
+string proname;
+string antname;
+
+Console.WriteLine("Välkommen till det första slagsmålsspelet!");
+Console.ReadLine();
+Console.WriteLine("Vad ska din protagonist heta?");
+proname = Console.ReadLine();
+
+Console.WriteLine("Vad ska din antagonist heta?");
+antname = Console.ReadLine();
+
+Console.WriteLine($"{proname} & {antname} ska slåss");
+while (prohp > 0 && anthp > 0)
+
+{
+    Console.WriteLine($"Runda {NO}");
+    NO ++;
+    Console.WriteLine($"\x1b[32m{proname}: {prohp}HP \x1b[31m{antname}: {anthp}HP");
+    int prodmg = Random.Shared.Next(5, 21);
+    prohp -= prodmg;
+    int antdmg = Random.Shared.Next(5, 21);
+    anthp -= antdmg;
+    
+    Console.WriteLine($"\x1b[32m{proname} slår {antname} och gör {antdmg} skada!");
+    Console.WriteLine($"\x1b[31m{antname} slår {proname} och gör {prodmg} skada!");
+    
+    Console.ReadLine();
+}
+if (prohp < 0 && anthp < 0)
+{
+    Console.WriteLine("\x1b[39mDet blev oavgjort, ingen vann!.");
+}
+else if (prohp < 0)
+{
+    Console.WriteLine($"\x1b[32m{proname} vann!");
+}
+
+else if (anthp < 0)
+{
+    Console.WriteLine($"\x1b[31m{antname} vann!");
+}
+
+Console.ReadLine();
